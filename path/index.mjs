a6bf775d47f55d4f2f17a14263c399949a8949f2
Ezequiel Miranda <MIRANDAEZQUIEL@GMAIL.COM>
2025-05-27 10:27:30 -0300

    ejemplo de uso de OS

--- path/index.mjs
+++ path/index.mjs
@@ -1,16 +1,12 @@
 import path from "path";
 
 const file_path = './data/ejemplo.txt';
-const dir_name = path.dirname(file_path);
-console.log('Directorio base'.dir_name);
+console.log('Directorio base: '.path.dirname(file_path));
 
-const base_name = path.basename(file_path, '.txt');
-console.log('Nombre del archivo:', base_name);
+console.log('Nombre del archivo: ', path.basename(file_path, '.txt'));
 
 // Obtener la extensión del archivo
-const ext_name = path.extname(file_path);
-console.log('Extensión del archivo:', ext_name);
+console.log('Extensión del archivo: ', path.extname(file_path));
 
 // Crear una ruta unida
-const new_path = path.join('/user', 'docs', 'newfile.txt');
-console.log('Nueva ruta:', new_path);
+console.log('Nueva ruta: ', path.join('/user', 'docs', 'newfile.txt'));
